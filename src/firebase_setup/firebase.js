c7f9efbf0e27ef39cc489a9187bd2e077498629e
Christian Fredrik <cjohnsen41@gmail.com>
2024-02-11 23:35:09 +0100

    Commented out unused imports to avoid warnings.

--- src/firebase_setup/firebase.js
+++ src/firebase_setup/firebase.js
@@ -1,12 +1,17 @@
 // Import the functions you need from the SDKs you need
-import { getAnalytics } from "firebase/analytics";
+
+// import { getAnalytics } from "firebase/analytics"; TODO: Uncomment this line if you want to use analytics, right now it is just giving a warning in the console
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+/*
+TODO: Add SDKs for Firebase products that you want to use
+https://firebase.google.com/docs/web/setup#available-libraries
+
+Your web app's Firebase configuration
+For Firebase JS SDK v7.20.0 and later, measurementId is optional
+*/
+
 const firebaseConfig = {
   apiKey: "AIzaSyBXE-Phv00ZXP07Stl2sanuiPNjO0dDDxc",
   authDomain: "reiseinspirasjon-b2579.firebaseapp.com",
@@ -19,6 +24,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app); TODO: Uncomment this line if you want to use analytics, right now it is just giving a warning in the console
 
 export const db = getFirestore(app);
