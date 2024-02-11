e9a33af86c9c690054456fd81818f9ef29f385d6
Christian Fredrik <cjohnsen41@gmail.com>
2024-02-12 00:13:22 +0100

    Refactor AddDestinationButton and TestPage components

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,11 +1,15 @@
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../firebase_setup/firebase";
 import { useState } from "react";
+import AddDestinationButton from "../components/AddDestinationButton";
 
 
 
 const TestPage = () => {
-  const [text, setText] = useState("")
+  
+  const [text, setText] = useState("") // State to store the input value
+  
+  // Function to send data to the database
   const sendData = async (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     try {
@@ -18,14 +22,19 @@ const TestPage = () => {
       console.error("Error submiting", e)
     }
   }
+
+  /* 
+  The return statement contains the JSX that will be rendered to the DOM
+  So essentially here we are returning the HTML that will be displayed on the page
+  */
   return (
     <>
-      <h1>Hello world!</h1>
       <form>
         <input type = "text" onChange={(e) => setText(e.target.value)}></input>
         <button onClick={sendData}>Click here!</button>
       </form>
-      {/* New Button */}
+      {/* AddDestinationButton is a component that we created in the components folder */}
+      <AddDestinationButton className="createButton"></AddDestinationButton>
     </>
   );
 };
