612571edb1afb7c78a55de394e328319a0b5bd26
Christopher Høe <christopher.hoee@gmail.com>
2024-02-15 18:51:32 +0100

    fix: fixed routing - Moved frontpage logic to App.tsx - All styles for frontpage should now be in App.css - Routing moved to index.tsx - update color and design for searchbar

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import DestinationPage from "./pages/DestinationPage";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
@@ -10,7 +12,12 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-      <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/destinationpage" element={<DestinationPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
