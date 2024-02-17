1d19fc5cc7bc82edf9e07b748e18d0727b81b954
Kristiantvaa <kraa1611@gmail.com>
2024-02-17 14:33:37 +0100

    Added a lot of stuff. Made a reusable component for the photos and descriptions. These can be styled further.

--- src/pages/DestinationPage.tsx
+++ src/pages/DestinationPage.tsx
@@ -1,10 +1,17 @@
 
 // import { useState } from "react";
 import "../style/DestinationPage.css";
-import CityPhoto from "./paris.jpg"; // Tell webpack this JS file uses this image
+import MainPhotoImg from "./paris.jpg"; // Tell webpack this JS file uses this image
+import attraction1 from "./eiffelAttraction.jpg"
+import attraction2 from "./arcDeTriumph.jpg"
+import attraction3 from "./louvre.jpg"
 
-// console.log(cityPhoto);
+import photo1 from "./champsElysees.jpg"
+import photo2 from "./notreDame.jpg"
+import photo3 from "./montmartre.jpg"
+import photo4 from "./seineRiver.jpg"
 
+import Slider from "../components/Slider";
 
 
 const DestinationPage = () => {
@@ -12,11 +19,15 @@ const DestinationPage = () => {
         
         // <div><Navbar></Navbar></div>
         <div>
-        <PhotoOfCity/>
+        <MainPhoto/>
         <TitleDiv/>
         <AllRatings/>
+        <div className="AllContentDivs">
         <DescriptionDiv/>
-        <Activites/>
+        <ActivitesDiv title="Ting å gjøre" activities={actualActivities}/>
+        <ActivitesDiv title="Bilder" activities={otherPhotosOfCity}/>
+    
+        </div>
         </div>
     );
 }
@@ -94,10 +105,10 @@ const TempRating = () => {
 
 
 
-const PhotoOfCity = () => {
+const MainPhoto = () => {
     return (
         <div className="PhotoOfCity">
-        <img src={CityPhoto} alt="Photo of city"/>;
+        <img src={MainPhotoImg} alt="Photo of city"/>;
         </div>
     )
 }
@@ -115,37 +126,147 @@ const TitleDiv = (props: Object) => {
 
 const DescriptionDiv = () => {
     return (
-        <div>
+        <div className="DescriptionDiv">
             <h3>Beskrivelse</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Autem facere ex cum expedita aliquam eum, rem dignissimos molestiae 
-                blanditiis dolore sint beatae repellat quam! Deserunt assumenda reiciendis 
-                pariatur inventore et!</p>
+            <p className="DescriptionText">
+                Paris, kjent som "Lysets by" (La Ville Lumière), er en av verdens mest ikoniske og romantiske destinasjoner. Med sin blendende arkitektur, kulturrike historie, førsteklasses shopping, utsøkt mat og pulserende kunstmiljø tiltrekker Paris millioner av besøkende hvert år. Utforsk ikoniske landemerker som Eiffeltårnet, Louvre-museet og Triumfbuen, nyt en avslappende spasertur langs Seinen eller utforsk sjarmerende nabolag som Montmartre og Le Marais. Paris tilbyr noe for enhver smak og er en by som garanterer uforglemmelige opplevelser for alle som besøker.
+            </p>
         </div>
     )
 }
 
-const Activites = () => {
-    let thingsToDo = {
-        football : "Football"
-    };
+
+
+
+type ActivitiesDivProps = {
+    title: string;
+    activities: Array<object>;
+}
+
+// Things to do
+
+let actualActivities : Array<object> = [
+    {caption : "Bilde av Eiffeltårnet", 
+    description : "Du kan kjøpe en billett, og gå trappene eller ta heisen opp til toppen.",
+    imgLink : attraction1},
+
+    {caption : "Bilde av Triumfbuen", 
+    description : "Du kan gå under den eller kjøpe en billett for å komme deg opp til toppen.",
+    imgLink : attraction2},
+    
+    {caption : "Bilde av Louvre", 
+    description : "Du må kjøpe billett for å komme inn. Blant de mange kunstverkene som er å finne her er Mona Lisa av Leonardo Da Vinci.", 
+    imgLink : attraction3}
+]
+
+// Other city photos
+
+let otherPhotosOfCity : Array<object> = [
+    {caption : "Bilde av Champs-Élysées", 
+    description : "", 
+    imgLink : photo1},
+
+    {caption : "Bilde av Notre Dame-katedralen", 
+    description : "", 
+    imgLink : photo2},
+    
+    {caption : "Bilde av Montmartre", 
+    description : "", 
+    imgLink : photo3},
+
+    {caption : "Bilde av Seinen",
+    description: "",
+    imgLink: photo4
+    }
+]
+
+const ActivitesDiv = (props: ActivitiesDivProps) => {    
     return (
-        <div className="activityDescriptions">
-        <h3>Ting å gjøre</h3>
-        <ActivityBox/>
-        <ActivityBox/>
-        <ActivityBox/>
+        <div className="ActivitiesDiv">
+            <h3>{props.title}</h3>
+            <div className="AllActivities">
+                {props.activities.map((activity: any, index:number) => 
+                <ActivityBox 
+                    key={index}
+                    caption={activity.caption}
+                    description={activity.description}
+                    imgLink={activity.imgLink}
+                />
+                )}
+            {/* <ActivityBox caption={props.activities[0].caption} description={props.activities[0].description} imgLink={props.activities[0].imgLink}/>
+             */}
+            </div>
         </div>
     );
-} 
+}
 
-const ActivityBox = (props:Object) => {
+type BoxProps = {
+    caption : string;
+    description : string;
+    imgLink : string;
+  };
+
+const ActivityBox = (props: BoxProps) => {
     return (
         <div className="ActivityBox">
-            <span>Fotball</span>
+            <img src={props.imgLink} alt={props.caption} id="imgAttraction"/>
+            <h4>{props.caption}</h4>
+            <p id="ImageDescription">{props.description}</p>
         </div>
     );
 }
 
 
+
+
+// type ActivitiesDivProps = {
+//     title: string;
+//     captions: object;
+//     descriptions: object;
+//     imgLinks: object;
+
+// }
+
+// const ActivitesDiv = () => {    
+//     return (
+//         <div className="ActivitiesDiv">
+//             <h3>Ting å gjøre</h3>
+//             <div className="AllActivities">
+//             <ActivityBox text="Photo of the Eiffel tower" imgLink={attraction1}/>
+//             <ActivityBox text="Photo of Champs-Elysees" imgLink={attraction2}/>
+//             <ActivityBox text="Photo of the Louvre" imgLink={attraction3}/>
+//             {/* <Slider></Slider> */}
+//             </div>
+//         </div>
+//     );
+// }
+
+// type ActivityBoxProps = {
+//     text: string;
+//     imgLink: string;
+//   };
+
+// const ActivityBox = (props: ActivityBoxProps) => {
+//     return (
+//         <div className="ActivityBox">
+//             <img src={props.imgLink} alt={props.text} id="imgAttraction"/>
+//             <p>{props.text}</p>
+//         </div>
+//     );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export default DestinationPage
+
