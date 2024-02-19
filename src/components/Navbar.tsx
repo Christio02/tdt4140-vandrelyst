573bff621a1aee0dd3a8a40953a462309633b2bd
Ane Christine Kanestrom <ane.c.kane@gmail.com>
2024-02-19 15:31:03 +0100

    link navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,13 +6,14 @@ import logo_navn from "../assets/logo_navn.png";
 import { CircleUserRound } from 'lucide-react';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import { Link } from 'react-router-dom';
   
   
 //nav bar full screen
 const Navbar = () => {
     return (
     <div id="topnav">
-        <a id = "logo" href="#home"> <img src = {logo_navn} alt="Logo i navbar" ></img> </a>
+        <Link id = "logo" to="/"> <img src = {logo_navn} alt="Logo i navbar" ></img> </Link>
         
         <a> <DropdownButton
                 title={<CircleUserRound size={24} />}
@@ -25,7 +26,7 @@ const Navbar = () => {
                 
             </DropdownButton> </a>
     
-        <a href="#about">Om oss</a>
+        <Link to="#about">Om oss</Link>
         
         </div>
     
