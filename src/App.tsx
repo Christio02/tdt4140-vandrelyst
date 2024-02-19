03797a09cd0ee998b66421feb0179c6913b8704e
Christopher Høe <christopher.hoee@gmail.com>
2024-02-19 14:09:59 +0100

    fix: fixed conflicts when merging

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { Link } from "react-router-dom";
 import "./App.css";
 import CardContainer from "./components/CardContainer";
@@ -13,14 +11,8 @@ import Searchbar from "./components/Searchbar";
 
 function App() {
   return (
-<<<<<<< HEAD
     <div className="main-container">
       <Navbar />
-      {/* AddDestinationButton is a component that we created in the components folder */}
-=======
-    <div className="App">
-      {<TestPage /> }
->>>>>>> 3947a58 (navbar set up)
       <Searchbar />
 
       <DestinationPopUp />
@@ -29,34 +21,6 @@ function App() {
       <CardContainer />
       <Footer />
     </div>
-=======
-import { Route, Routes } from "react-router-dom";
-=======
-import { Link } from "react-router-dom";
->>>>>>> 4cac580 (fix: fixed routing)
-import "./App.css";
-import DestinationPopUp from "./components/DestinationPopUp";
-import Searchbar from "./components/Searchbar";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-function App() {
-  return (
-<<<<<<< HEAD
-    <Routes>
-      <Route path="/" element={<Frontpage />} />
-      <Route path="/destinationpage" element={<DestinationPage />} />
-    </Routes>
->>>>>>> b4bf6dc (add routes for links)
-=======
-    <div className="main-container">
-      {/* AddDestinationButton is a component that we created in the components folder */}
-      <Searchbar />
-
-      <DestinationPopUp />
-      <Link to="/destinationpage">Destination</Link>
-    </div>
->>>>>>> 4cac580 (fix: fixed routing)
   );
 }
-
 export default App;
