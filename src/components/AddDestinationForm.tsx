00bb2ebd70b639d23284f790321b8acb54c25e1b
Christopher Høe <christopher.hoee@gmail.com>
2024-03-11 09:21:32 +0100

    feat: continued on update destination

--- src/components/AddDestinationForm.tsx
+++ src/components/AddDestinationForm.tsx
@@ -1,4 +1,4 @@
-import { addDoc, collection, doc, getDoc } from "firebase/firestore";
+import { addDoc, collection } from "firebase/firestore";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import React, { useEffect, useState } from "react";
 import { Col, Dropdown, Row } from "react-bootstrap";
@@ -20,12 +20,8 @@ import { LucideCircleFadingPlus } from 'lucide-react';
  * Provides an option to upload an image for the destination.
  * Saves the destination data to a database upon user submission.
  */
-interface AddDestinationFormProps {
-  destination?: Destination;
-}
-const AddDestinationForm: React.FC<AddDestinationFormProps> = ({
-  destination,
-}) => {
+
+const AddDestinationForm = (isUpdate: boolean) => {
   const [showAddDestination, setShowAddDestination] = useState(false);
 
   const location = useLocation();
@@ -246,153 +242,139 @@ const AddDestinationForm: React.FC<AddDestinationFormProps> = ({
     return Array(end - start + 1)
       .fill(0)
       .map((_, idx) => start + idx);
+
+  function range(start: number, end: number) {
+    // Function to create a range of numbers
+    return Array(end - start + 1)
+      .fill(0)
+      .map((_, idx) => start + idx);
   }
 
-  const RenderDestinationForm = () => {
-    return (
-      <>
-        {!isOnDestination && (
-          <Button
-            className="NewDest"
-            variant="primary"
-            onClick={handleAddShow}
-          > <LucideCircleFadingPlus size={25} id="icon" />
-            Ny destinasjon
-          </Button>
-        )}
-        {showAddDestination && !isOnDestination && (
-          <>
-            <div className="modal-container">
-              <Modal
-                show={showAddDestination}
-                onHide={handleAddClose}
-                size="xl"
-              >
-                {/* from https://react-bootstrap.netlify.app/docs/components/modal */}
-                <Modal.Header closeButton>
-                  {/* Top bar, where the X is.*/}
-                  <Modal.Title className="ms-auto">
-                    Skjema for oppretting av destinasjon
+  return (
+    <>
+      {/* <AddDestinationButton className="createButton"></AddDestinationButton> */}
+      <Button
+        className="createButton"
+        variant="primary"
+        onClick={handleAddShow}
+      >
+        Legg til destinasjon
+      </Button>
+
+      {showAddDestination && (
+        <div className="modal-container">
+          <Modal show={showAddDestination} onHide={handleAddClose} size="xl">
+            {/* from https://react-bootstrap.netlify.app/docs/components/modal */}
+            <Modal.Header closeButton>
+              {/* Top bar, where the X is.*/}
+              <Modal.Title className="ms-auto">
+                Skjema for oppretting av destinasjon
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body as={"div"} style={{ padding: "1rem" }}>
+              <Row className="mb-1 justify-content-center">
+                <Col>
+                  <Modal.Title className="text-center">
+                    Generell informasjon
                   </Modal.Title>
-                </Modal.Header>
-                <Modal.Body as={"div"} style={{ padding: "1rem" }}>
-                  <Row className="mb-1 justify-content-center">
-                    <Col>
-                      <Modal.Title className="text-center">
-                        Generell informasjon
-                      </Modal.Title>
-                    </Col>
-                  </Row>
-                  <Row className="mb-1">
-                    <Form.Group
-                      controlId="formFile"
-                      className="destination-file"
-                    >
-                      {/* For image upload*/}
-                      <Form.Label>Forsidebilde</Form.Label>
-                      <Form.Control
-                        type="file"
-                        size="lg"
-                        onChange={handleImageChange}
-                      />
-                    </Form.Group>
-                  </Row>
-                  <Row className="mb-1">
-                    <Col md={2}>
-                      <InputGroup>
-                        <Form.Control
-                          type="text"
-                          placeholder="By"
-                          onChange={handleCityChange}
-                          autoFocus
-                        />
-                      </InputGroup>
-                    </Col>
-                    <Col md={2}>
-                      <InputGroup>
-                        <Form.Control
-                          type="text"
-                          placeholder="Land"
-                          onChange={handleCountryChange}
-                        />
-                      </InputGroup>
-                    </Col>
-                    <Col md={2}>git 
-                      <InputGroup>
-                        <Form.Select
-                          onChange={handleTypeChange}
-                          defaultValue=""
-                        >
-                          <option value="" disabled>
-                            Type
-                          </option>
-                          {["Vinter", "Natur", "Storby", "Strand"].map(
-                            (type) => (
-                              <option key={type} value={type}>
-                                {type}
-                              </option>
-                            )
-                          )}
-                        </Form.Select>
-                      </InputGroup>
-                    </Col>
-                    <Col md={2}>
-                      <InputGroup>
-                        <Form.Select
-                          onChange={handleRatingChange}
-                          defaultValue=""
-                        >
-                          <option value="" disabled>
-                            Rating
-                          </option>
-                          {range(0, 5).map((rating) => (
-                            <option key={rating} value={rating}>
-                              {rating}
-                            </option>
-                          ))}
-                        </Form.Select>
-                      </InputGroup>
-                    </Col>
-                    <Col md={2}>
-                      <InputGroup>
-                        <Form.Select
-                          onChange={handlePriceChange}
-                          defaultValue=""
-                        >
-                          <option value="" disabled>
-                            Pris
-                          </option>
-                          {range(0, 5).map((price) => (
-                            <option key={price} value={price}>
-                              {price}
-                            </option>
-                          ))}
-                        </Form.Select>
-                      </InputGroup>
-                    </Col>
-                    <Col md={2}>
-                      <InputGroup>
-                        <Form.Control
-                          type="text"
-                          placeholder="Temperatur"
-                          onChange={handleTemperatureChange}
-                        />
-                      </InputGroup>
-                    </Col>
-                  </Row>
-                  {/* Description */}
-                  <Row className="mb-5">
-                    <Col>
-                      <Form.Group controlId="description">
-                        <Form.Label>Beskrivelse</Form.Label>
-                        <Form.Control
-                          as="textarea"
-                          rows={3}
-                          onChange={handleDescriptionChange}
-                        ></Form.Control>
-                      </Form.Group>
-                    </Col>
-                  </Row>
+                </Col>
+              </Row>
+              <Row className="mb-1">
+                <Form.Group controlId="formFile" className="destination-file">
+                  {/* For image upload*/}
+                  <Form.Label>Forsidebilde</Form.Label>
+                  <Form.Control
+                    type="file"
+                    size="lg"
+                    onChange={handleImageChange}
+                  />
+                </Form.Group>
+              </Row>
+              <Row className="mb-1">
+                <Col md={2}>
+                  <InputGroup>
+                    <Form.Control
+                      type="text"
+                      placeholder="By"
+                      onChange={handleCityChange}
+                      autoFocus
+                    />
+                  </InputGroup>
+                </Col>
+                <Col md={2}>
+                  <InputGroup>
+                    <Form.Control
+                      type="text"
+                      placeholder="Land"
+                      onChange={handleCountryChange}
+                    />
+                  </InputGroup>
+                </Col>
+                <Col md={2}>
+                  <InputGroup>
+                    <Form.Select onChange={handleTypeChange} defaultValue="">
+                      <option value="" disabled>
+                        Type
+                      </option>
+                      {["Vinter", "Natur", "Storby", "Strand"].map((type) => (
+                        <option key={type} value={type}>
+                          {type}
+                        </option>
+                      ))}
+                    </Form.Select>
+                  </InputGroup>
+                </Col>
+                <Col md={2}>
+                  <InputGroup>
+                    <Form.Select onChange={handleRatingChange} defaultValue="">
+                      <option value="" disabled>
+                        Rating
+                      </option>
+                      {range(0, 5).map((rating) => (
+                        <option key={rating} value={rating}>
+                          {rating}
+                        </option>
+                      ))}
+                    </Form.Select>
+                  </InputGroup>
+                </Col>
+                <Col md={2}>
+                  <InputGroup>
+                    <Form.Select onChange={handlePriceChange} defaultValue="">
+                      <option value="" disabled>
+                        Pris
+                      </option>
+                      {range(0, 5).map((price) => (
+                        <option key={price} value={price}>
+                          {price}
+                        </option>
+                      ))}
+                    </Form.Select>
+                  </InputGroup>
+                </Col>
+                <Col md={2}>
+                  <InputGroup>
+                    <Form.Control
+                      type="text"
+                      placeholder="Temperatur"
+                      onChange={handleTemperatureChange}
+                    />
+                  </InputGroup>
+                </Col>
+              </Row>
+              {/* Description */}
+              <Row className="mb-5">
+                <Col>
+                  <Form.Group controlId="description">
+                    <Form.Label>Beskrivelse</Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      rows={3}
+                      onChange={handleDescriptionChange}
+                    ></Form.Control>
+                  </Form.Group>
+                </Col>
+              </Row>
 
                   <Row className="mb-1 justify-content-center">
                     <Col>
@@ -982,5 +964,4 @@ const AddDestinationForm: React.FC<AddDestinationFormProps> = ({
     </>
   );
 };
-
 export default AddDestinationForm;
